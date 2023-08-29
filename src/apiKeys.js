1274ea359b3886dfa36a58cc367756cec003067f
rishu0805 <103452730+rishu0805@users.noreply.github.com>
2023-08-29 23:07:05 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "9168db0821580adeeb356747d2d81484",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
